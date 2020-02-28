d181333ef542bc6b516c035e19c7d2edb3b01aa3
Muhammad Usama <usamamuhammad100@gmail.com>
2020-02-28 20:30:34 +0500

    routes Demo Created

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require("express");
-
+const connectDb = require("./config/db");
 const app = express();
 
+// connect DB
+connectDb();
+
 app.get("/", (req, res) => res.send("Api Running"));
+
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/post", require("./routes/api/post"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
