b99fc5113c1d8935d7c6183f66df0f56e0a08c09
Muhammad Usama <usamamuhammad100@gmail.com>
2020-03-01 12:44:39 +0500

    get Log In profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const route = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
-route.get("/", (req, res) => res.send("profile"));
+// route Get api/profile/me
+
+route.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id
+    }).populate("user", ["name", "avater"]);
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+    res.json(profile);
+  } catch (e) {
+    console.error(e.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = route;
