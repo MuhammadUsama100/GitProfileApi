974ac54b3d75e8eb05be3d9372b28c31af3fb499
Muhammad Usama <usamamuhammad100@gmail.com>
2020-03-04 11:26:23 +0500

    Create Post and Get Post

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,6 +1,55 @@
 const express = require("express");
 const route = express.Router();
+const { check, validationResult } = require("express-validator/check");
+const auth = require("../../middleware/auth");
 
-route.get("/", (req, res) => res.send("post"));
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+//post create Post methord
+route.post(
+  "/",
+  [
+    auth,
+    [
+      check("text", "Text is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.status(400).json({ errors: error.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+      const post = await newPost.save();
+      res.json(post);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+// get request to get all post
+
+route.get("/", auth, async (req, res) => {
+  try {
+    const post = await Post.find().sort({ date: -1 }); // new first
+    res.json(post);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = route;
