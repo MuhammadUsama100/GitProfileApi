bc6956c411c828141633bceb26c7ec122aa2ea7d
Muhammad Usama <usamamuhammad100@gmail.com>
2020-03-05 14:17:58 +0500

    Router Set

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,23 @@ import React, { Fragment } from "react";
 import "./App.css";
 import Navbar from "./component/Navbar";
 import Landing from "./component/Landing";
+import Register from "./component/auth/register";
+import Login from "./component/auth/login";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
   );
 };
 
